6357cba382296cfccd41d52974d36b5775641ead
minagishl <mvxd568.co@icloud.com>
2025-04-02 18:11:14 +0900

    fix: correct type definitions

--- src/@types/discord.d.ts
+++ src/@types/discord.d.ts
@@ -1,6 +1,8 @@
-import type { Collection } from "discord.js";
+import { Collection } from "discord.js";
+import { SlashCommand } from "./commands";
+
 declare module "discord.js" {
   export interface Client {
-    commands: Collection<unknown, unknown>;
+    commands: Collection<string, SlashCommand>;
   }
 }
